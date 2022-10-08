49ccd87602512962fc33899402400ba0f17a131a
adiaz-dev <adiaz.noriega@gmail.com>
2022-10-08 16:17:49 -0500

    Compute two numbers

--- src/main/java/org/augusto/App.java
+++ src/main/java/org/augusto/App.java
@@ -2,6 +2,7 @@ package org.augusto;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("This is my commit!!!");
+        int a = 5+6;
+        System.out.println("This is my commit!!!" + a);
     }
 }
