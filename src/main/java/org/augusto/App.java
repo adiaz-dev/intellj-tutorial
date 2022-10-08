bbcc02198676be26fe6b69cae864c1d908140c16
adiaz-dev <adiaz.noriega@gmail.com>
2022-10-08 16:22:01 -0500

    Beatifully the code

--- src/main/java/org/augusto/App.java
+++ src/main/java/org/augusto/App.java
@@ -3,6 +3,6 @@ package org.augusto;
 public class App {
     public static void main(String[] args) {
         int a = 5+6;
-        System.out.println("This is my commit!!!" + a);
+        System.out.println("This is my commit: " + a);
     }
 }
